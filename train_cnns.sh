c429583674e2ad8cbf4bddcdde8c8449d6779aa0
Pablo Hernandez <pablo.hernandezdo.13@sansano.usm.cl>
2021-06-17 01:18:49 -0400

    Train 9 models on paralel

--- train_cnns.sh
+++ train_cnns.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # CNN1
-echo "Training model CNN_6k_6k, lr = 1e-3, epochs = 20, batch_size = 256"
+echo "Training model CNN1, lr = 1e-3, epochs = 20, batch_size = 256"
 python train.py \
         --lr 1e-3 \
         --epochs 20 \
@@ -13,4 +13,135 @@ python train.py \
         --model_name CNN1_1e3_256 \
         --dataset_name "STEAD-ZEROS" \
         --train_path "Data/TrainReady/Train_constant.npy" \
+        --val_path "Data/TrainReady/Val_constant.npy" &
+
+P1=$!
+
+echo "Training model CNN1, lr = 1e-4, epochs = 20, batch_size = 256"
+python train.py \
+        --lr 1e-4 \
+        --epochs 20 \
+        --batch_size 256 \
+        --earlystop 0 \
+        --eval_iter 1 \
+        --model_folder 'models'  \
+        --classifier CNN1 \
+        --model_name CNN1_1e4_256 \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/Train_constant.npy" \
+        --val_path "Data/TrainReady/Val_constant.npy" &
+
+P2=$!
+
+echo "Training model CNN1, lr = 1e-5, epochs = 20, batch_size = 256"
+python train.py \
+        --lr 1e-5 \
+        --epochs 20 \
+        --batch_size 256 \
+        --earlystop 0 \
+        --eval_iter 1 \
+        --model_folder 'models'  \
+        --classifier CNN1 \
+        --model_name CNN1_1e5_256 \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/Train_constant.npy" \
+        --val_path "Data/TrainReady/Val_constant.npy" &
+
+P3=$!
+
+echo "Training model CNN2, lr = 1e-3, epochs = 20, batch_size = 256"
+python train.py \
+        --lr 1e-3 \
+        --epochs 20 \
+        --batch_size 256 \
+        --earlystop 0 \
+        --eval_iter 1 \
+        --model_folder 'models'  \
+        --classifier CNN2 \
+        --model_name CNN2_1e3_256 \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/Train_constant.npy" \
+        --val_path "Data/TrainReady/Val_constant.npy" &
+
+P4=$!
+
+echo "Training model CNN2, lr = 1e-4, epochs = 20, batch_size = 256"
+python train.py \
+        --lr 1e-4 \
+        --epochs 20 \
+        --batch_size 256 \
+        --earlystop 0 \
+        --eval_iter 1 \
+        --model_folder 'models'  \
+        --classifier CNN2 \
+        --model_name CNN2_1e4_256 \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/Train_constant.npy" \
+        --val_path "Data/TrainReady/Val_constant.npy" &
+
+P5=$!
+
+echo "Training model CNN2, lr = 1e-5, epochs = 20, batch_size = 256"
+python train.py \
+        --lr 1e-5 \
+        --epochs 20 \
+        --batch_size 256 \
+        --earlystop 0 \
+        --eval_iter 1 \
+        --model_folder 'models'  \
+        --classifier CNN2 \
+        --model_name CNN2_1e5_256 \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/Train_constant.npy" \
+        --val_path "Data/TrainReady/Val_constant.npy" &
+
+P6=$!
+
+echo "Training model CNN3, lr = 1e-3, epochs = 20, batch_size = 256"
+python train.py \
+        --lr 1e-3 \
+        --epochs 20 \
+        --batch_size 256 \
+        --earlystop 0 \
+        --eval_iter 1 \
+        --model_folder 'models'  \
+        --classifier CNN3 \
+        --model_name CNN3_1e3_256 \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/Train_constant.npy" \
+        --val_path "Data/TrainReady/Val_constant.npy" &
+
+P7=$!
+
+echo "Training model CNN3, lr = 1e-4, epochs = 20, batch_size = 256"
+python train.py \
+        --lr 1e-4 \
+        --epochs 20 \
+        --batch_size 256 \
+        --earlystop 0 \
+        --eval_iter 1 \
+        --model_folder 'models'  \
+        --classifier CNN3 \
+        --model_name CNN3_1e4_256 \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/Train_constant.npy" \
+        --val_path "Data/TrainReady/Val_constant.npy" &
+
+P8=$!
+
+echo "Training model CNN3, lr = 1e-5, epochs = 20, batch_size = 256"
+python train.py \
+        --lr 1e-5 \
+        --epochs 20 \
+        --batch_size 256 \
+        --earlystop 0 \
+        --eval_iter 1 \
+        --model_folder 'models'  \
+        --classifier CNN3 \
+        --model_name CNN3_1e5_256 \
+        --dataset_name "STEAD-ZEROS" \
+        --train_path "Data/TrainReady/Train_constant.npy" \
         --val_path "Data/TrainReady/Val_constant.npy"
+
+P9=$!
+wait $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9
